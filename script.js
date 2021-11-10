fb0c5057f6fada0fa10447071fbb770f96a36ac9
بندر عبدالرحمن برنية <barniyah@ADM.GOV.SA>
2021-11-10 08:53:06 +0300

    lodaer

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const quoteText  = document.getElementById('quote');
 const authorText  = document.getElementById('author');
 const twitterBtn  = document.getElementById('twitter');
 const newQuoteBtn  = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading -- oppesite of Loading function
+function complete() {
+quoteContainer.hidden = false;
+loader.hidden = true;
+}
+
 // Show New Random Quote
 function newQuote() {
+    loading();
     // pick a random quote . . 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // console.log(quote);
@@ -25,11 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -46,3 +62,4 @@ newQuoteBtn.addEventListener('click', newQuote);
 
 // on Load 
 getQuotes();
+// loading();
